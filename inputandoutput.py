76b80cbd7d77db1322f125720e97b6d2eee5088d
rubanizamkhan <rubanizamkhan@gmail.com>
2026-01-11 14:49:32 +0530

    Add program to perform arithmetic operations
    Implement basic arithmetic operations on two numbers.

--- inputandoutput.py
+++ inputandoutput.py
@@ -1,1 +1,18 @@
+# Write a python program to read two numbers and display their sum, difference, product and quotient
 
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+
+sum=a+b
+diff=a-b
+pro=a*b
+
+print("Sum is:", sum)
+print("Diff is:", diff)
+print("Pro is:", pro)
+
+if b!=0:
+    quo=a/b
+    print("Quotient is:", quo)
+else:
+    print("Divison is not possible")
